67da7177f23311e7aa9d33190a98b507dc754cff
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-04-22 01:17:48 -0500

    SoyMaycol

--- settings.js
+++ settings.js
@@ -58,13 +58,13 @@ global.avatar = 'https://i.postimg.cc/W1P24C13/descarga-24.jpg'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
-global.gp1 = 'https://chat.whatsapp.com/CDw7hpI30WjCyKFAVLHNhZ'
-global.comunidad1 = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa'
-global.channel = 'https://whatsapp.com/channel/0029VbAfPu9BqbrEMFWXKE0d'
-global.channel2 = 'https://whatsapp.com/channel/0029VbAfPu9BqbrEMFWXKE0d'
-global.md = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot'
-global.correo = 'thekingdestroy507@gmail.com'
-global.cn ='https://whatsapp.com/channel/0029VapSIvR5EjxsD1B7hU3T';
+global.gp1 = 'https://wa.me/51921826291'
+global.comunidad1 = 'https://whatsapp.com/channel/0029VayXJte65yD6LQGiRB0R'
+global.channel = 'https://whatsapp.com/channel/0029VayXJte65yD6LQGiRB0R'
+global.channel2 = 'https://whatsapp.com/channel/0029VayXJte65yD6LQGiRB0R'
+global.md = 'https://github.com/SoySapo6/SigmaBot'
+global.correo = 'karatekidamericatv@gmail.com'
+global.cn ='https://whatsapp.com/channel/0029VayXJte65yD6LQGiRB0R';
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
